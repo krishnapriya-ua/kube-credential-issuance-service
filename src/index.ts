625da0addd2ce6638cd19b05ed63a9bbbdf50554
krishnapriya U.A <krishnapriyaua@gmail.com>
2025-10-12 21:35:22 +0530

    PORT Change to env

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import issuanceRoot from './routes/issuanceRoutes.js'
 import './config.js'
 import dotenv from 'dotenv'
 const app = express();
-const port = "3000";
+const port = process.env.PORT;
 dotenv.config()
 app.use(express.json())
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://kube-credential.kptech.online'],
     methods: 'GET,POST,PUT,PATCH,DELETE,HEAD',
     credentials:true
 }))
